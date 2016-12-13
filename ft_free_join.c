2a8968333f357be2b3f0859d7d342ddfe7e0c539
Victor KANNEMACHER <vkannema@e1r9p11.42.fr>
2016-12-13 15:23:25 +0100

    add gnl

--- ft_free_join.c
+++ ft_free_join.c
@@ -6,27 +6,31 @@
 /*   By: vkannema <vkannema@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/10 10:43:10 by vkannema          #+#    #+#             */
-/*   Updated: 2016/12/10 10:48:16 by vkannema         ###   ########.fr       */
+/*   Updated: 2016/12/13 14:39:39 by vkannema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_free_join(char *dest, char *buff, int i)
+char	*ft_free_join(char *s1, char *s2, int i)
 {
 	char	*tmp;
 
-	if (!(tmp = ft_strjoin(dest, buff)))
+	if (!(tmp = ft_strjoin(s1, s2)))
 		return (NULL);
-	if (i == 1)
-		free(dest);
-	if (i == 2)
-		free(buff);
 	if (i == 0)
 	{
-		free(dest);
-		free(buff);
+		ft_memdel((void **)&s1);
 	}
-	dest = NULL;
+	if (i == 1)
+	{
+		ft_memdel((void **)&s2);
+	}
+	if (i == 2)
+	{
+		ft_memdel((void **)&s1);
+		ft_memdel((void **)&s2);
+		return (tmp);
+	}
 	return (tmp);
 }
